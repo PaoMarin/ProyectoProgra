9654637e8ceca8bad7d726f0438ed96b696b2bd9
PaoMarin <paomg.9614@gmail.com>
2017-03-16 19:01:17 -0600

    Clase Model

---
+++ Tiempos/Model/Apuesta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Tiempos.Model
+{
+    class Apuesta : DBAccess.ErrorHandler
+    {
+        public int id { get; set; }
+        public double monto { get; set; }
+        public int numero { get; set; }
+        public DateTime fecha { get; set; }
+
+        public Apuesta()
+        {
+
+        }
+
+        public Apuesta(int id, double monto, int numero, DateTime fecha)
+        {
+            this.id = id;
+            this.monto = monto;
+            this.numero = numero;
+            this.fecha = fecha;
+        }
+
+        public DataTable Select(int id, double monto, int numero, DateTime fecha)
+        {
+            Dictionary<double, object> parametros = new Dictionary<double, object>();
+            StringBuilder sql_builder = new StringBuilder();
+
+            string sql = "select * from apuesta";
+            if (parametros.Count > 0)
+            {
+
+                sql += "where " + sql_builder.ToString();
+            }
+
+            DataTable result = Program.da.SqlQuery(sql, parametros);
+            if (Program.da.isError)
+            {
+                this.isError = true;
+                this.errorDescription = Program.da.errorDescription;
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Tiempos/Model/Ganador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiempos.Model
+{
+    class Ganador
+    {
+        public int id { get; set; }
+        public int premio1 { get; set; }
+        public int premio2 { get; set; }
+        public int premio3 { get; set; }
+    }
+}
